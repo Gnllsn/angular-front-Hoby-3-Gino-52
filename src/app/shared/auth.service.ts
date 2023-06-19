4e8edca452cbf5f826a7aa643dd6d88545963fa2
Gino Allison <77733677+Gnllsn@users.noreply.github.com>
2023-06-19 20:53:50 +0300

    Label Connexion

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,38 +1,49 @@
 import { Injectable } from '@angular/core';
+import { StorageService } from './storage.service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthService {
-  loggedIn = false;
+	loggedIn = false;
+	
+	constructor(private storageService : StorageService) { }
+	
+	isActive(){
+		return localStorage.getItem("data")!=null ; 
+	}
 
-  constructor() { }
-
-  // théoriquement, on devrait passer en paramètre le login
-  // et le password, cette méthode devrait faire une requête
-  // vers un Web Service pour vérifier que c'est ok, renvoyer
-  // un token d'authentification JWT etc.
-  // elle devrait renvoyer un Observable etc.
-  logIn() {
-    console.log("ON SE LOGGE")
-    this.loggedIn = true;
-  }
-
-  logOut() {
-    console.log("ON SE DELOGGE")
-
-    this.loggedIn = false;
-  }
-
-  // si on l'utilisait on ferai isAdmin().then(...)
-  isAdmin() {
-    // Pour le moment, version simplifiée...
-    // on suppose qu'on est admin si on est loggué
-    const isUserAdminPromise = new Promise((resolve, reject) => {
-        resolve(this.loggedIn);
-    });
-
-    // on renvoie la promesse qui dit si on est admin ou pas
-    return isUserAdminPromise;
-  }
+	logout(){
+		localStorage.removeItem("data") ;
+		localStorage.setItem("labelConnexion","Se connecter");	
+		window.location.reload() ;
+	}
+	
+	// théoriquement, on devrait passer en paramètre le login
+	// et le password, cette méthode devrait faire une requête
+	// vers un Web Service pour vérifier que c'est ok, renvoyer
+	// un token d'authentification JWT etc.
+	// elle devrait renvoyer un Observable etc.
+	logIn() {
+		console.log("ON SE LOGGE")
+		this.loggedIn = true;
+	}
+	
+	logOut() {
+		console.log("ON SE DELOGGE")
+		
+		this.loggedIn = false;
+	}
+	
+	// si on l'utilisait on ferai isAdmin().then(...)
+	isAdmin() {
+		// Pour le moment, version simplifiée...
+		// on suppose qu'on est admin si on est loggué
+		const isUserAdminPromise = new Promise((resolve, reject) => {
+			resolve(this.loggedIn);
+		});
+		
+		// on renvoie la promesse qui dit si on est admin ou pas
+		return isUserAdminPromise;
+	}
 }
