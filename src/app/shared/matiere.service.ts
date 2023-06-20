92bfd99fc9727a01d12ab4512b15ceac9d5211c1
Hobyy19 <arivonyhoby@gmail.com>
2023-06-20 20:47:48 +0300

    Add authorization to matiere

--- src/app/shared/matiere.service.ts
+++ src/app/shared/matiere.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, forkJoin, map, of, tap } from 'rxjs';
 import { Matiere } from '../models/matiere.model';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatiereService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient , private storageService : StorageService ) { }
 
   // uri_api = 'http://localhost:8010/api/matieres';
     uri_api = 'https://back-api-nfim.onrender.com/api/matieres';
 
   getMatieres():Observable<any> {
-    return this.http.get<Matiere[]>(this.uri_api);
+    const options = this.storageService.formOptionJSON(true,this.storageService.getStorage().token) ;
+    return this.http.get<Matiere[]>(this.uri_api,options);
   }
 
 }
